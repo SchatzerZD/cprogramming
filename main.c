5515c12c9e2116a69143eb24bfdf0e9021d743f3
Schatzer ZD <daniel.walaker@gmail.com>
2022-11-10 12:20:04 +0100

    Testing out conversion characters

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
+
+
+
     printf("Hello, World!\n");
+    printf("%s is a person\n","Daniel");
+    printf("%s is %d years old\n","Daniel",21);
+
+    //printf("yo" + " hi") does not work
+
+
+
     return 0;
 }
