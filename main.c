1b05d78a25cbfdca53896f483e61e15665d18daa
Schatzer ZD <daniel.walaker@gmail.com>
2022-11-10 12:25:38 +0100

    Testing out variables

--- main.c
+++ main.c
@@ -3,10 +3,11 @@
 int main() {
 
 
+    int age = 21;
 
     printf("Hello, World!\n");
     printf("%s is a person\n","Daniel");
-    printf("%s is %d years old\n","Daniel",21);
+    printf("%s is %d years old\n","Daniel",age);
 
     //printf("yo" + " hi") does not work
 
